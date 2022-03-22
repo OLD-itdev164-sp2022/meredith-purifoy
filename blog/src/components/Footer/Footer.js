927894ef0d954d7ca6273349e4e6dfa729134f61
MeredithPurifoy <purifoml@gmatc.matc.edu>
2022-03-22 11:22:43 -0500

    Checkpoint 2

---
+++ blog/src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { BaseContainer } from '../BaseContainer'
+
+export const Footer = props => {
+  return (
+    <BaseContainer as="footer" {...props} />
+  )
+}
---
+++ blog/src/components/Main/Main.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { BaseContainer } from '../BaseContainer'
+
+export const Main = props => {
+  return (
+    <BaseContainer as="main" {...props} />
+  )
+}
