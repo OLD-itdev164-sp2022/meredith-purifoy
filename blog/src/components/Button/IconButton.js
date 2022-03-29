754b9a965e4560e3c68d9ab5f32f5d35fae48834
MeredithPurifoy <purifoml@gmatc.matc.edu>
2022-03-29 12:23:46 -0500

    Completed Actuvuty 8

--- blog/src/components/Button/IconButton.js
+++ blog/src/components/Button/IconButton.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import { BaseButton } from './BaseButton'
 
 const StyledButton = styled(BaseButton)`
-    // Inject later
+    ${ ( { theme, variant }) => theme.variants.iconButton[variant || 'primary']}
 `
 
 export const IconButton = styled(({ icon, ...rest }) => {
@@ -21,5 +21,6 @@ IconButton.defaultProps = {
 }
 
 IconButton.propTypes = {
-    icon: PropTypes.node.isRequired
+    icon: PropTypes.node.isRequired,
+    variant: PropTypes.string
 }
